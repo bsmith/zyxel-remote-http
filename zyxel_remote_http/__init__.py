1604b5a14a49bfc0e70ae3e90ff82d35dc596970
Benjamin Smith <bsmith@cabbage.org.uk>
2023-07-31 22:12:39 +0100

    Object oriented command issuing

--- zyxel_remote_http/__init__.py
+++ zyxel_remote_http/__init__.py
@@ -1,13 +1,3 @@
 
 from .zyxel import Zyxel
-
-from .login_v260 import performLogin as performLogin_v260
-from .login_v270 import performLogin as performLogin_v270
-
-def performLogin(session, url, username, password, version):
-    if version == "260":
-        performLogin_v260(session, url, username, password)
-    elif version == "270":
-        performLogin_v270(session, url, username, password)
-    else:
-        raise Exception("Not sure about version %s" % version)
+from .login import performLogin
--- zyxel_remote_http/zyxel.py
+++ zyxel_remote_http/zyxel.py
@@ -1,10 +1,21 @@
+from .common import make_session, zyxelUrl
+from .login import performLogin
+
 class Zyxel():
     def __init__(self, host, fwversion):
         self.host = host
         self.fwversion = fwversion
+        self.session = make_session()
+        self.url_base = zyxelUrl(host)
 
     def login(self, user, password):
-        pass
+        performLogin(self.session, self.url_base, user, password, self.fwversion)
 
     def cmd(self, cmd):
-        pass
+        ret = self.session.get(self.url_base, params={"cmd": cmd})
+        if ret.ok:
+            print(ret)
+            print(ret.text)
+        else:
+            raise Exception("Failed call cmd %s."
+                            "Got response: %s" % (cmd, ret.text))
